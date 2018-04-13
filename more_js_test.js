3e9eeae0d295d0def1b07ead53c040f9d7f9c9c9
Logan Weir <loganweir@gmail.com>
2018-04-13 07:46:52 -0700

    updated html

--- more_js_test.js
+++ more_js_test.js
@@ -1,1 +1,4 @@
-d3.select("g").text("Hello World");
+var circle = d3.selectAll("circle");
+circle.style("fill", "steelblue");
+circle.attr("r", 20);
+circle.attr("cx", function() { return Math.random() * 720; });
